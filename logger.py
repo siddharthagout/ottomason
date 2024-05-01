1ea4db01b4c917387f0ceb5bdda1708793e248d8
Siddhartha Goutam <siddharthagout@gmail.com>
2024-05-01 18:59:14 +0530

    changed logger config

--- logger.py
+++ logger.py
@@ -3,5 +3,5 @@ import logging.config
 import os
 
 logger = logging.getLogger('dev')
-with open("/Users/sid/next-company-prep/ottomason/logging.ini", "r") as config_file:
+with open("logging.ini", "r") as config_file:
     logging.config.fileConfig(config_file)
